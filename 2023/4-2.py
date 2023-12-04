18b68eb65e3eb2a9b7549a232f87a264ee2e55cb
marcmelis <marc.melis@hayanmind.com>
2023-12-04 12:49:09 +0100

    day 4

---
+++ 2023/4-2.py
@@ -0,0 +1,13 @@
+total = 0
+with open('data/4.txt') as f:
+    repetitions = [1 for i in range(len(f.readlines()))]
+with open('data/4.txt') as f:
+    for i, line in enumerate(f):
+        winning, numbers = line.split('|')
+        winning =  [n for n in winning.strip().split(' ') if n != ""]
+        numbers = [n for n in numbers.strip().split(' ') if n != ""]
+        winners = [n for n in numbers if n in winning]
+        for j in range(i+1, i+len(winners)+1):
+            if j < len(repetitions):
+                repetitions[j] += repetitions[i]
+print(sum(repetitions))

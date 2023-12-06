6d213489e4bf2329f3606c39752391d0c4a4367e
marcmelis <marc.melis@hayanmind.com>
2023-12-06 17:49:47 +0100

    day 6 without bruteforce on p2

--- 2023/day6/6-2.py
+++ 2023/day6/6-2.py
@@ -1,10 +1,8 @@
-# Not good
 with open('6.txt') as f:
     time, distance = f.readlines()
-    time = int(time.split(':')[1].strip().replace(' ',''))
-    record_distance = int(distance.split(':')[1].strip().replace(' ',''))
-    wins = 0
-    for i in range(time+1):
-        distance = i *(time - i)
-        if distance > record_distance: wins += 1
-    print(wins)
+    t = int(time.split(':')[1].strip().replace(' ',''))
+    D = int(distance.split(':')[1].strip().replace(' ',''))
+    limit1 = (-t + (t**2-(4*D))**0.5) / -2
+    limit2 = (-t - (t**2-(4*D))**0.5) / -2
+    if limit1 == int(limit1): limit1 += 1
+    print(int(limit2)-int(limit1))
